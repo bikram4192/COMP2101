79813f7e12501cda484be04fefb8c7018c7ec0c3
bikram4192 <bikram4192@gmail.com>
2019-05-14 12:47:33 -0500

    created hello world scripts

--- bash/helloworld.sh
+++ bash/helloworld.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "hello world"
-exit
+echo "i am a process # $$"
---
+++ bash/helloworldtemplated.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# my second script - helloworldtemplate.sh
+
+cat << EOF
+hello world
+i am a process # $$
+EOF
